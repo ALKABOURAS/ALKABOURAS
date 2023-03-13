b6436725055f72028398f68a81b35691489f9e58
ALKABOURAS <66868213+ALKABOURAS@users.noreply.github.com>
2023-03-13 10:46:57 +0000

    υυυ

--- question01.py
+++ question01.py
@@ -3,15 +3,24 @@ import re
 f=open("weather.txt", mode="r", encoding="utf8")
 content=f.read()
 f.close()
-reg=3
+reg=8
 match reg:
     case 1:
-        "[0-9]"
+        pattern=re.compile ("[0-9]")
     case 2:
-        "[0-9]+\.?[0-9]?"
+        pattern=re.compile("[0-9]+\.?[0-9]?")
     case 3:
-        ""
-pattern=re.compile(reg)  # <-- Εδώ βάζουμε το RE
+        pattern=re.compile("[0-9]+\.?[0-9]?\s[m]+")  # <-- Εδώ βάζουμε το RE
+    case 4:
+        pattern=re.compile("\((.*?)\)")
+    case 5:
+        pattern=re.compile("[0-3]?[0-9]\/[01]?[0-9]\/[012]?[0-9]?[0-9][0-9]")
+    case 6:
+        pattern=re.compile("[0-ω]+\s[0-3]?[0-9]\/[01]?[0-9]\/[012]?[0-9]?[0-9][0-9]")
+    case 7:
+        pattern=re.compile("[0-ω]*[άήόύώέίϋϊΆΉΌΎΏΈΊ][0-ω]*")
+    case 8:
+        pattern=re.compile("[Α-ΩΆΉΌΎΏΈΊ][0-ωάήόύώέίϋϊ][0-ωάήόύώέίϋϊ]+")
 result=pattern.findall(content)
 print(result)
 print(len(result))
