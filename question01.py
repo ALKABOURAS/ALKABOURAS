1392985de6d8e2b8006c7c7be3c24eda56ca51cb
ALKABOURAS <66868213+ALKABOURAS@users.noreply.github.com>
2023-03-13 10:46:53 +0000

    φφφ

--- question01.py
+++ question01.py
@@ -3,8 +3,15 @@ import re
 f=open("weather.txt", mode="r", encoding="utf8")
 content=f.read()
 f.close()
-
-pattern=re.compile("[0-9]")  # <-- Εδώ βάζουμε το RE
+reg=3
+match reg:
+    case 1:
+        "[0-9]"
+    case 2:
+        "[0-9]+\.?[0-9]?"
+    case 3:
+        ""
+pattern=re.compile(reg)  # <-- Εδώ βάζουμε το RE
 result=pattern.findall(content)
 print(result)
 print(len(result))
